477cbf8ec495f24b5337d6689434e4404c000b2e
tota <rabbetota@gmail.com>
2025-09-09 02:39:51 +0600

    backend url added

--- frontend/src/api/ResumeService.js
+++ frontend/src/api/ResumeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURLL = "http://localhost:8080";
+export const baseURLL = "https://ai-resume-maker-2gwm.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: baseURLL,
